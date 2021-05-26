6097f718e605b059660c92e944b140ddd8b2aed3
ssep4u <shyoon63@e-mirim.hs.kr>
2021-05-26 10:20:17 +0900

    jat/models verbose_name_plural, deadline, access

--- jat/models.py
+++ jat/models.py
@@ -4,10 +4,13 @@ from django.db import models
 class Repository(models.Model):
     name = models.CharField(max_length=40)
     description = models.CharField(max_length=200)
+    deadline = models.DateTimeField()
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
     # repo1.introduction_set
+    class Meta:
+        verbose_name_plural = 'Repositories'
 
     def __str__(self):
         return self.name
@@ -17,6 +20,7 @@ class Introduction(models.Model):
     repository = models.ForeignKey(Repository, on_delete=models.CASCADE)  # intro1.repository
     version = models.IntegerField(default=1)
     contents = models.TextField()
+    access = models.IntegerField(default=1) #0: private, 1: public
 
     # intro1.comment_set
 
