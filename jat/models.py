6b364a8339057159e60130c19f84ab277c9e25a6
ssep4u <shyoon63@e-mirim.hs.kr>
2021-05-26 10:08:31 +0900

    jat/models Repository, Introduction, Comment

--- jat/models.py
+++ jat/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class Repository(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    # repo1.introduction_set
+
+    def __str__(self):
+        return self.name
+
+
+class Introduction(models.Model):
+    repository = models.ForeignKey(Repository, on_delete=models.CASCADE)  # intro1.repository
+    version = models.IntegerField(default=1)
+    contents = models.TextField()
+
+    # intro1.comment_set
+
+    def __str__(self):
+        return f'{self.version} {self.contents}'
+
+
+class Comment(models.Model):
+    introduction = models.ForeignKey(Introduction, on_delete=models.CASCADE)  # comm1.introduction
+    comment = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.comment
