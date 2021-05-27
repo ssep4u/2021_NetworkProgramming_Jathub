80271d774167eaf48ebe5e2d802ed7f6f406c3af
ssep4u <shyoon63@e-mirim.hs.kr>
2021-05-27 10:20:41 +0900

    jat/views IntroductionDetailView

--- jat/views.py
+++ jat/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 
-from jat.models import Repository
+from jat.models import Repository, Introduction
 
 
 class RepositoryListView(generic.ListView):
@@ -10,3 +10,7 @@ class RepositoryListView(generic.ListView):
 
 class RepositoryDetailView(generic.DetailView):
     model = Repository
+
+
+class IntroductionDetailView(generic.DetailView):
+    model = Introduction
