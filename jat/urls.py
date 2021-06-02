678410877be3bc3205bdeaadbaa7a2d32c6de376
ssep4u <shyoon63@e-mirim.hs.kr>
2021-06-02 09:14:56 +0900

    jat/urls introduction_detail

--- jat/urls.py
+++ jat/urls.py
@@ -7,4 +7,5 @@ app_name = 'jat'
 urlpatterns = [
     path('', views.RepositoryListView.as_view(), name='repository_list'),    #jat:repository_list
     path('repository/<int:pk>/', views.RepositoryDetailView.as_view(), name='repository_detail'),    #jat:repository_detail
+    path('repository/<int:repository_pk>/introduction/<int:pk>/', views.IntroductionDetailView.as_view(), name='introduction_detail'),    #jat:introduction_detail
 ]
