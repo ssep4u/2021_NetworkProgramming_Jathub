1a098a55c8f7791ecd4f66a86ed2e7f53bfdc7e3
ssep4u <shyoon63@e-mirim.hs.kr>
2021-06-02 10:01:25 +0900

    jat/views RepositoryCreateView

--- jat/views.py
+++ jat/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from jat.models import Repository, Introduction
@@ -10,6 +11,13 @@ class RepositoryListView(generic.ListView):
 
 class RepositoryDetailView(generic.DetailView):
     model = Repository
+
+
+class RepositoryCreateView(generic.CreateView):
+    model = Repository
+    fields = ['name', 'description', 'deadline']    #'__all__'
+    template_name_suffix = '_create'
+    success_url = reverse_lazy('jat:repository_list')
 
 
 class IntroductionDetailView(generic.DetailView):
