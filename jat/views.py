82240ee4fcc96aa8d1e52399abffef0636c71455
ssep4u <shyoon63@e-mirim.hs.kr>
2021-06-03 09:48:53 +0900

    jat/views IntroductionCreateView, IntroductionUpdateView, IntroductionDeleteView

--- jat/views.py
+++ jat/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.db.models import Max
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy
 from django.views import generic
 
@@ -34,3 +35,40 @@ class RepositoryDeleteView(generic.DeleteView):
 
 class IntroductionDetailView(generic.DetailView):
     model = Introduction
+
+
+class IntroductionCreateView(generic.CreateView):
+    model = Introduction
+    fields = ['repository', 'version', 'contents', 'access']  # '__all__'
+    template_name_suffix = '_create'
+
+    # success_url = reverse_lazy('jat:repository_detail') #repository_detail은 pk가 필요함(ImproperlyConfigured at /repository/3/introduction/add/)
+    def get_initial(self):
+        repository = get_object_or_404(Repository, pk=self.kwargs['repository_pk'])
+        introduction = repository.introduction_set.aggregate(
+            Max('version'))  # 해당 repository의 introduction 중 version 최대값 구하자
+        version = introduction['version__max']
+        if version == None:  # introduction이 아예 없으면 version 기본값: 1
+            version = 1
+        else:  # introduction이 있으면 version 최대값에서 +1
+            version += 1
+        return {'repository': repository, 'version': version}
+
+    def get_success_url(self):
+        return reverse_lazy('jat:repository_detail', kwargs={'pk': self.kwargs['repository_pk']})
+
+
+class IntroductionUpdateView(generic.UpdateView):
+    model = Introduction
+    fields = ['repository', 'version', 'contents', 'access']  # '__all__'
+    template_name_suffix = '_update'
+
+    def get_success_url(self):
+        return reverse_lazy('jat:repository_detail', kwargs={'pk': self.kwargs['repository_pk']})
+
+
+class IntroductionDeleteView(generic.DeleteView):
+    model = Introduction
+
+    def get_success_url(self):
+        return reverse_lazy('jat:repository_detail', kwargs={'pk': self.kwargs['repository_pk']})
